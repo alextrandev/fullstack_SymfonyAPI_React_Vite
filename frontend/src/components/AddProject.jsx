d0533c6b993d3a8010e606233c4f8670cdbccb88
dk10119 <tranduclong092@gmail.com>
2024-05-14 14:11:30 +0300

    Feat: add a form render and handle submit function to post data to db

--- frontend/src/components/AddProject.jsx
+++ frontend/src/components/AddProject.jsx
@@ -1,5 +1,40 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+
 export default function AddProject() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const navigate = useNavigate(); // Create an instance of useHistory
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:8007/api/projects', { name, description })
+      .then(res => {
+        console.log('Success:', res.data);
+        navigate('/projects'); // Navigate to the project list page
+      })
+      .catch((error) => console.error('Error:', error));
+  };
+
   return (
-    <div>AddProject</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <h1>Add Project</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        placeholder="Project Name"
+      />
+      <input
+        type="text"
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+        placeholder="Project Description"
+      />
+      <button type="submit">Add Project</button>
+      <Link to={"/projects"}>Show all projects</Link>
+    </form>
+  );
 }
